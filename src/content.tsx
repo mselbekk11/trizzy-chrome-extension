a4a0c16fb7bbcd944f3f83572a41e107962c612d
mselbekk11 <mselbekk11@gmail.com>
2025-04-07 15:10:48 -0700

    updates

--- src/content.tsx
+++ src/content.tsx
@@ -1,7 +1,7 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
 
-import { CountButton } from "~features/count-button"
+// import { CountButton } from "~features/count-button"
 
 export const config: PlasmoCSConfig = {
   matches: ["<all_urls>"]
@@ -40,7 +40,7 @@ export const getStyle = (): HTMLStyleElement => {
 const PlasmoOverlay = () => {
   return (
     <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
+      {/* <CountButton /> */}
     </div>
   )
 }
