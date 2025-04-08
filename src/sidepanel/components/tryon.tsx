c517792b40a0718f3c8c342a76a521b1c7032ea5
mselbekk11 <mselbekk11@gmail.com>
2025-04-08 09:50:37 -0700

    nope

--- src/sidepanel/components/tryon.tsx
+++ src/sidepanel/components/tryon.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { useUser } from "@clerk/chrome-extension"
 
 export const TryOn = () => {
@@ -6,6 +7,7 @@ export const TryOn = () => {
   return (
     <div className="plasmo-flex-1 plasmo-flex plasmo-flex-col plasmo-items-center plasmo-bg-[#F9F9F9] plasmo-p-4 plasmo-overflow-auto plasmo-min-h-0 ">
       <div className="plasmo-mb-4">Welcome, {user?.firstName || "User"}!</div>
+      <Button variant="default" className="plasmo-bg-black plasmo-text-white plasmo-px-4 plasmo-py-2 plasmo-rounded-lg">Click me</Button>
     </div>
   )
 }
