ed585e1c337e052608b93ba96e231ef72344782d
mselbekk11 <mselbekk11@gmail.com>
2025-04-08 12:03:45 -0700

    updates

---
+++ install-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script ensures dependencies are installed before running the dev server
+
+echo "Installing dependencies..."
+npm install
+
+echo "Running development server..."
+npm run dev 
